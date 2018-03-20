b96f66c8c387d4bbdb12ce996cde4bf1f6807012
Johanna Khenkittisak <jkhenkittisak@beapi.fr>
2018-03-20 18:50:30 +0100

    Filter apartments

--- src/ApartmentList.js
+++ src/ApartmentList.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Apartment from './Apartment';
 import apartments from './apartments.json';
 
-const ApartmentList = (apartments) => {
-	return(
-		<div> {apartments.map(Apartments => <Apartment apartments={apartments}/>)} </div>
-	)
-
+class ApartmentList extends Component{
+		state = {
+       showAll: false
+    };
+    showToggle = () => {
+	    this.showAll = !this.showAll;
+ 		}
+ 	render(){
+ 		return(
+			<div>
+				<button onClick={this.showToggle}>
+				{}
+				{this.state.showAll ? "Show only available" : "Show all"}
+				</button>
+				
+				{apartments.map(apartment => <Apartment apartment={apartment}/>)}
+				{ show &&
+					apartments.map(apartment => <Apartment apartment={apartment}/>)
+				}
+				{ !showAll &&
+					apartments.filter(apartments => aprtments.isAvailable).map(apartment => <Apartment apartment={apartment}/>)
+				}
+				{apartments.map(apartment =>
+				(apartments.isAvailable || showAll === "true") && (
+					<Apartment apartment = {apartment }/>
+				)
+				)}
+			</div>
+			
+		)
+ 	}
 }
-
-
 
 export default ApartmentList;
 
